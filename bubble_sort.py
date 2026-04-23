fd5b29c6e6c24f20f30abbe45005f439240531ec
柿子 <wangshiqin@MacBook-Pro-150.local>
2026-04-23 16:11:51 +0800

    init

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,9 +3,13 @@ data = [12, 4, 56, 21, 43, 67]
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
+        swapped = False
         for j in range(n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
 
 bubble_sort(data)
 print(data)
